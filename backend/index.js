63135840df5d2df0090933dd6e084307988a40b3
jinwoo <yasisicoco@gmail.com>
2025-08-11 02:16:35 +0900

    feat: 관리자계정 모델 설계 및 라이브러리 설치

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("연결 성공"))
+  .catch((e) => console.log(e, ": 실패"));
 
 app.listen(PORT, () => {
   console.log("Server is running");
