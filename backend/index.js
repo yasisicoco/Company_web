8bdd3f874dd723c1ef2d2a2749396009705dfe31
Jinwoo <yasisicoco@gmail.com>
2025-08-12 12:57:10 +0900

    feat: 관리자 계정 로그인/로그아웃/삭제 기능추가

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser"); // express 에서 쿠키 해석을 돕는 서드파티
 const app = express();
 const PORT = 3000;
 
@@ -8,8 +9,9 @@ const userRoutes = require("./routes/user");
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cookieParser());
 
-app.use("/api/auth", userRoutes);
+app.use("/api/auth", userRoutes); // 다중요청 분산처리
 
 app.get("/", (req, res) => {
   res.send("Hello world");
@@ -17,7 +19,7 @@ app.get("/", (req, res) => {
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("연결 성공"))
+  .then(() => console.log("MONGE DB 연결 성공!"))
   .catch((e) => console.log(e, ": 실패"));
 
 app.listen(PORT, () => {
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const axios = require("axios");
+const jwt = require("jsonwebtoken");
 
 router.post("/signup", async (req, res) => {
   try {
@@ -24,6 +26,136 @@ router.post("/signup", async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: "서버 오류가 발생했습니다." });
     console.log(error);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username }).select("+password");
+
+    if (!user) {
+      return res.status(401).json({ message: "사용자를 찾을 수 없습니다." });
+    }
+
+    // 비활성화계정
+    if (!user.isActive) {
+      return res
+        .status(401)
+        .json({ message: "비활성화된 계정입니다. 관리자에게 문의하세요." });
+    }
+
+    // 중복 로그인 방지
+    if (user.isLoggedIn) {
+      return res
+        .status(401)
+        .json({ message: "이미 다른 기기에서 로그인되어 있습니다." });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      user.failedLoginAttempts += 1;
+      user.lastLoginAttempt = new Date();
+
+      if (user.failedLoginAttempts >= 5) {
+        user.isActive = false;
+        await user.save(); // 해당 유저의 정보를 DB로 업데이트 시킴. 실시간 업데이트
+        return res.status(401).json({
+          message: "비밀번호를 5회 이상 틀려 계정이 비활성화 되었습니다.",
+        });
+      }
+
+      await user.save();
+      return res.status(401).json({
+        message: "비밀번호가 일치하지 않습니다.",
+        remainingAttemps: 5 - user.failedLoginAttempts,
+      });
+    }
+
+    user.failedLoginAttempts = 0;
+    user.lastLoginAttempt = new Date();
+    user.isLoggedIn = true;
+
+    // 유저 IP 업데이트
+    try {
+      const response = await axios.get("https://api.ipify.org?format=json");
+      const ipAddress = response.data.ip;
+      user.ipAddress = ipAddress;
+    } catch (error) {
+      console.log("IP 주소를 가져오던 중 오류 발생: ", error.message);
+    }
+
+    await user.save();
+
+    const token = jwt.sign(
+      { userId: user._id, username: user.username },
+      process.env.JWT_SECRET,
+      { expiresIn: "8h" }
+    );
+
+    console.log("토큰값: ", token);
+
+    res.cookie("token", token, {
+      httpOnly: true, //자바스크립트로 토큰 가져오지 못하게
+      secure: false,
+      sameSite: "strict", //크로스사이트
+      maxAge: 24 * 60 * 60 * 1000, // 토큰이 삭제되는 시간(24h)
+    });
+
+    //패스워드 지우고 보내기
+    const userWithoutPassword = user.toObject();
+    delete userWithoutPassword.password;
+
+    res.json({ user: userWithoutPassword });
+  } catch (error) {
+    console.log("서버오류: ", error.message);
+    res.status(500).json({ message: "서버오류가 발생했습니다." });
+  }
+});
+
+router.post("/logout", async (req, res) => {
+  try {
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.status(400).json({ message: "이미 로그아웃된 상태입니다." });
+    }
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await User.findById(decoded.userId);
+
+      if (user) {
+        user.isLoggedIn = false;
+        await user.save();
+      }
+    } catch (error) {
+      console.log("토큰 검증 오류: ", error.message);
+    }
+
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+    });
+
+    res.json({ message: "로그아웃 되었습니다." });
+  } catch (error) {
+    console.log("로그아웃 오류: ", error.message);
+    res.status(500).json({ message: "서버 오류가 발생했습니다." });
+  }
+});
+
+router.delete("/delete/:userId", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.userId);
+    if (!user) {
+      return res.status(404).json({ message: "사용자를 찾을 수 없습니다." });
+    }
+    res.json({ message: "사용자가 성공적으로 삭제되었습니다." });
+  } catch (error) {
+    res.status(500).json({ message: "서버 오류가 발생했습니다." });
   }
 });
 
